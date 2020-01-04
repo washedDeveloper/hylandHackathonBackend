ac4685bb7e7e453d137b81af514bbbc628446818
Jimmy <jimmystry@gmail.com>
2020-01-04 04:12:07 -0500

    initial dump of basic details complete: working

--- src/routes/databaseroutes/personData.js
+++ src/routes/databaseroutes/personData.js
@@ -2,14 +2,20 @@ const routes = require('express').Router();
 
 const dbhandler = require("../../databaseHandler");
 routes.get('/:userID', async (req, res) => {
+
   dbhandler.getUserData(req.params.userID).then( userData => {
+
     const userDetails = {
       username: userData.Username,
       name: userData.Name,
       email: userData.Email
     }
+
+    //dbhandler.getUserSchool(userData.SchoolID);
+
     console.log(userDetails);
-    res.send("Hello")
+    res.status(200).send({ userDetails });
+
   }).catch( err => {
     console.error(err);
   });
